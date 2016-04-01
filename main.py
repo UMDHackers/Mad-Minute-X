62bba92224dffb05da6898b8203e6873d44d049a
Parth Desai <PDesai@FCMD.local>
2016-04-01 14:01:55 -0400

    logo

--- main.py
+++ main.py
@@ -7,31 +7,32 @@ def index():
 @app.route("/<section>/easy")
 def easy(section):
     if section == "add":
-        return render_template
+        return "easy"
     elif section == "subtract":
-
+        return "easy"
     elif section == "mulitplication":
-
+        return "easy"
     elif section == "division":
-
+        return "easy"
 @app.route("/<section>/medium")
 def medium(section):
     if section == "add":
-        return render_template
+        return "medium"
     elif section == "subtract":
-
+        return "medium"
     elif section == "mulitplication":
-
+        return "medium"
     elif section == "division":
+        return "medium"
 @app.route("/<section>/hard")
 def hard(section):
     if section == "add":
-        return render_template
+        return "hard"
     elif section == "subtract":
-
+        return "hard"
     elif section == "mulitplication":
-
+        return "hard"
     elif section == "division":
-
+        return "hard"
 if __name__ == "__main__":
     app.run(debug=True)
