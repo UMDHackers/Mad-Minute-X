40827e5eb12f82290a4b5a36cea737800d05c44f
Parth Desai <PDesai@FCMD.local>
2016-04-04 10:26:17 -0400

    index done working on test now

--- main.py
+++ main.py
@@ -6,33 +6,12 @@ def index():
     return render_template('index.html')
 @app.route("/<section>/easy")
 def easy(section):
-    if section == "add":
-        return "easy"
-    elif section == "subtract":
-        return "easy"
-    elif section == "mulitplication":
-        return "easy"
-    elif section == "division":
-        return "easy"
+    return render_template('test.html', section = section, level = "easy")
 @app.route("/<section>/medium")
 def medium(section):
-    if section == "add":
-        return "medium"
-    elif section == "subtract":
-        return "medium"
-    elif section == "mulitplication":
-        return "medium"
-    elif section == "division":
-        return "medium"
+    return render_template('test.html', section = section, level = "medium")
 @app.route("/<section>/hard")
 def hard(section):
-    if section == "add":
-        return "hard"
-    elif section == "subtract":
-        return "hard"
-    elif section == "mulitplication":
-        return "hard"
-    elif section == "division":
-        return "hard"
+    return render_template('test.html', section = section, level = "hard")
 if __name__ == "__main__":
     app.run(debug=True)
